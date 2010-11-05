e3492a63faf1f712628194deae8eeba5728c7f61
Aaron Tidwell <atidwell@marthastewart.com>
2010-11-05 19:31:02 -0400

    Initial stable alpha

--- application/classes/controller/getsongs.php
+++ application/classes/controller/getsongs.php
@@ -4,10 +4,20 @@ Class Controller_getsongs extends Controller
 {
     function action_index()
     {
+        $recache = $this->determine_recaching();
         $hmxDataHelper = new Model_HMXData;
         $view = View::factory('json');
-        $view->set('jsondata', $hmxDataHelper->getAllSongs());
+        
+        $view->set('jsondata', $hmxDataHelper->getAllSongs($recache));
         $view->set('callback', $_REQUEST['callback']);
         $this->request->response = $view;
+    }
+    
+    private function determine_recaching() {
+      $recache = false;
+      if (isset($_REQUEST['recache']) && $_REQUEST['recache'] == 'true') {
+          $recache = true;
+      }
+      return $recache;
     }
 }
--- application/classes/model/hmxdata.php
+++ application/classes/model/hmxdata.php
@@ -4,28 +4,61 @@ class Model_HMXData extends Model {
     
   }
   
-  public function getAllSongs() {
-    //get the raw JSON
-    $url = 'http://rockband.com/services.php/music/all-songs.json';
-    $curl_handle=curl_init();
-    curl_setopt($curl_handle,CURLOPT_URL,$url);
-    curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
-    curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,1);
-    curl_setopt($curl_handle,CURLOPT_ENCODING , "gzip");
-    $result = curl_exec($curl_handle);
-    curl_close($curl_handle);
+  public function getAllSongs($recache = false) {
+    if ($recache || !self::cacheExists()) {
+      //get the raw JSON
+      $url = 'http://rockband.com/services.php/music/all-songs.json';
+      $curl_handle=curl_init();
+      curl_setopt($curl_handle,CURLOPT_URL,$url);
+      curl_setopt($curl_handle,CURLOPT_CONNECTTIMEOUT,2);
+      curl_setopt($curl_handle,CURLOPT_RETURNTRANSFER,1);
+      curl_setopt($curl_handle,CURLOPT_ENCODING , "gzip");
+      $result = curl_exec($curl_handle);
+      curl_close($curl_handle);
     
-    //decode it
+      //decode it
+      
+      //store the data
+      self::storeCache($result);
+    }
+    else {
+      $result = self::restoreCache();
+    }
     $decoded_data = json_decode($result);
-    
     return $decoded_data;
   }
   
-  public function storeCache($filename, $data) {
-    $fname = '/application/cache'.$filename.'.txt';
-    if (file_exists($fname)) {
-      $handle = fopen($fname, "r");
-      $data = fread($handle, filesize($fname));
+  //checks if a cache file exists
+  private function cacheExists() {
+    $file = $this->getCacheFileName();
+    if (file_exists($file)) {
+      return true;
     }
+    return false;
+  }
+  
+  //stores data in a file by serializing the data
+  private function storeCache($data) {
+    $file = $this->getCacheFileName();
+    $fp = fopen($file, 'w')  or die("can't open file");
+    fwrite($fp, $data);
+    fclose($fp);
+	}
+  
+  //reads the serialized data from a file, unserializes it, and returns
+  private function restoreCache() {
+    $file = $this->getCacheFileName();
+    if (file_exists($file)) {
+      $handle = fopen($file, "r");
+      $data = fread($handle, filesize($file));
+      return $data;
+    }
+    return false;
+  }
+  
+  //gets the name of the function that called the function that called this function (lols)
+  private function getCacheFileName() {
+    $bt = debug_backtrace();
+    return APPPATH.'cache/'.$bt[2]['function'].date('mdy').'.txt';   
   }
 }
